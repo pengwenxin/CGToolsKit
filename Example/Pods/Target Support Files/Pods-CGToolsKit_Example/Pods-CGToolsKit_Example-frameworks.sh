1fa81bc8f53219b77bba8f2c50370662f45bd188
pengwenxin <980120476@qq.com>
2018-08-23 10:12:23 +0800

    CGTools

--- Example/Pods/Target Support Files/Pods-CGToolsKit_Example/Pods-CGToolsKit_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-CGToolsKit_Example/Pods-CGToolsKit_Example-frameworks.sh
@@ -144,10 +144,12 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/CGToolsKit/CGToolsKit.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/FMDB/FMDB.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MBProgressHUD/MBProgressHUD.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/CGToolsKit/CGToolsKit.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/FMDB/FMDB.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MBProgressHUD/MBProgressHUD.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
